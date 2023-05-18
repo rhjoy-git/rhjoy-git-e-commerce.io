b2735cf47fdcbc90bde4e1b6382d21da1f076901
rakibulhasan-joy <rakibulhasanjoy009@gmail.com>
2023-05-18 11:57:46 +0600

    product description added

---
+++ assets/js/product-name.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 0
+        },
+        568: {
+            items: 3
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
