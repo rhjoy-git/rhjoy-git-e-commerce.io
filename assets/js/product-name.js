f8224547ff767c1a5993c19ee3e1ee2b381e7098
rakibulhasan-joy <rakibulhasanjoy009@gmail.com>
2023-05-25 12:19:28 +0600

    new modification

--- assets/js/product-name.js
+++ assets/js/product-name.js
@@ -1,20 +1,49 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    dots: false,
-    responsive: {
-        0: {
-            items: 0
-        },
-        568: {
-            items: 3
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 3
+$(document).ready(function () {
+    let productSlider = $("#product-slider");
+    let relatedProductSlider = $("#related-product-slider");
+    productSlider.owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ['<div class="slick-arrow prev"><span class="fa-solid fa-angles-left"></span></div>',
+                    '<div class="slick-arrow next"><span class="fa-solid fa-angles-right"></span></div>'],
+        dots: false,
+        responsive: {
+            0: {
+                items: 0
+            },
+            568: {
+                items: 3
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
         }
-    }
+    });
+    
+    relatedProductSlider.owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ['<div class="slick-arrow prev"><span class="fa-solid fa-angles-left"></span></div>',
+                    '<div class="slick-arrow next"><span class="fa-solid fa-angles-right"></span></div>'],
+        dots: false,
+        responsive: {
+            0: {
+                items: 0
+            },
+            568: {
+                items: 3
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
 })
--- style.js
+++ style.js
@@ -1,4 +1,68 @@
-// Rating Initialization
-$(document).ready(function() {
-    $('#rating').mdbRate();
-  });
+$(document).ready(function () {
+	"use strict";
+	// i. client (carousel)
+	$('#client').owlCarousel({
+		items: 4,
+		loop: true,
+		smartSpeed: 1000,
+		autoplay: true,
+		dots: false,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 2
+			},
+			415: {
+				items: 3
+			},
+			600: {
+				items: 3
+
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
+
+
+	$('.play').on('click', function () {
+		owl.trigger('play.owl.autoplay', [1000])
+	})
+	$('.stop').on('click', function () {
+		owl.trigger('stop.owl.autoplay')
+	});
+
+	// i. serviceOffer (carousel)
+	$('#serviceOffer').owlCarousel({
+		items: 4,
+		loop: true,
+		smartSpeed: 1000,
+		autoplay: true,
+		dots: false,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 2
+			},
+			415: {
+				items: 3
+			},
+			600: {
+				items: 3
+
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
+
+
+	$('.play').on('click', function () {
+		owl.trigger('play.owl.autoplay', [1000])
+	})
+	$('.stop').on('click', function () {
+		owl.trigger('stop.owl.autoplay')
+	})
+});
